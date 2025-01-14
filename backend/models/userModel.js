7e59d32958695934b5af057483612cfd5a1d029f
Tiago <tiago.abreu.laibacas@gmail.com>
2025-01-14 11:42:47 +0000

    ✨ Enhance user model with additional fields for personal information and roles

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
+const sexEnum = ["male", "female", "other"];
+const roleEnum = ["client", "worker", "admin"];
+const servicesEnum = [];
 const userSchema = new Schema({
   username: {
     type: String,
@@ -18,6 +19,59 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phonePrefix: {
+    type: String,
+    required: function () {
+      return this.phoneNumber != null;
+    },
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  serviceRate: {
+    type: Number,
+    default: 0,
+  },
+  rank: {
+    type: Number,
+    default: 0,
+  },
+  sex: {
+    type: String,
+    enum: sexEnum,
+    required: true,
+  },
+  birthDate: {
+    day: {
+      type: Number,
+      required: true,
+    },
+    month: {
+      type: Number,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+  },
+  serviceSpecialist: {
+    type: String,
+    enum: servicesEnum,
+  },
+  role: {
+    type: String,
+    enum: roleEnum,
+    default: "client",
   },
   createdAt: {
     type: Date,
