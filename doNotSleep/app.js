9972a97390910af0775cedd035f996019941be1a
Tiago <tiago.abreu.laibacas@gmail.com>
2025-01-16 11:34:08 +0000

    ✨ Update dependencies: bump versions for @types/node, electron-to-chromium, es-object-atoms, libphonenumber-js, mongodb-connection-string-url, tr46, whatwg-url, and adjust package.json accordingly

--- doNotSleep/app.js
+++ doNotSleep/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Permitir parsing de JSON no corpo das requisições
+app.use(express.json());
 
 // Routes
 app.use("/requests", requestRoutes);
