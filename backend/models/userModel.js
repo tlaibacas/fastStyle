07a6e23b02036ba316838f42e57f0ccb32c96251
Tiago <tiago.abreu.laibacas@gmail.com>
2025-01-27 14:57:13 +0000

    ✨ Add password hashing to user model: implement pre-save hook to hash password using authService

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const authService = require("../services/authService");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -21,6 +22,19 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    try {
+      this.password = await authService.hashPassword(this.password);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
