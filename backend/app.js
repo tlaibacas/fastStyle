20420e11bc5d28b9b35ee41257bffa119ca7cb2c
tlaibacas <tiago.abreu.laibacas@gmail.com>
2025-02-18 21:36:26 +0000

    refactor(app): add multer for file uploads and enhance body parsing middleware

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -7,12 +8,15 @@ const userRoutes = require("./routes/users");
 const loginRoutes = require("./routes/authRoutes");
 
 const app = express();
+const upload = multer();
 
 // Trust proxy settings (for Nginx, AWS Load Balancer, etc.)
 app.set("trust proxy", true);
 
 // Middleware
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.any());
 app.use(cors());
 app.use(morgan("dev"));
 
