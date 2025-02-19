a5aed77aa5401e9eca7aa5df079cfe2a5da2e36d
tlaibacas <tiago.abreu.laibacas@gmail.com>
2025-02-19 13:24:04 +0000

    refactor(jwtHelper): improve token generation and verification by validating payload and token format

--- backend/utils/jwtHelper.js
+++ backend/utils/jwtHelper.js
@@ -1,20 +1,19 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
 require("dotenv").config();
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
 // Function to generate a JWT token
-async function generateToken(userId, expiresIn = "30d") {
+function generateToken(payload, expiresIn = "30d") {
   try {
-    const user = await User.findById(userId);
-    if (!user) {
-      throw new Error("User not found");
+    if (!payload || typeof payload !== "object") {
+      throw new Error("Invalid payload.");
     }
 
-    const payload = {
-      userId: user._id,
-    };
+    if (!secretKey) {
+      throw new Error("JWT secret key not defined.");
+    }
+
     const token = jwt.sign(payload, secretKey, { expiresIn });
     return token;
   } catch (error) {
@@ -25,13 +24,14 @@ async function generateToken(userId, expiresIn = "30d") {
 // Function to verify and decode a JWT token
 function verifyToken(token) {
   try {
-    console.log("Verifying token:", token);
+    if (!token || typeof token !== "string") {
+      throw new Error("Invalid token.");
+    }
+
     const decoded = jwt.verify(token, secretKey);
-    console.log("Token verified:", decoded);
     return decoded;
   } catch (error) {
-    console.error("Invalid token:", error.message);
-    throw new Error("Invalid token");
+    throw new Error("Invalid token: " + error.message);
   }
 }
 
