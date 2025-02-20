ff9cbdb0658eb9cc4ecbd94f880d52710d4e8e71
tlaibacas <tiago.abreu.laibacas@gmail.com>
2025-02-20 15:24:56 +0000

    refactor(middleguard): enhance user role decryption and access control checks

--- backend/middleware/middleguard.js
+++ backend/middleware/middleguard.js
@@ -1,24 +1,29 @@
-// middleware/middleguard.js
 const User = require("../models/userModel");
 const cryptoHelper = require("../utils/cryptoHelper");
 
 const middleguard = (...allowedRoles) => {
   return async (req, res, next) => {
     try {
+      // Fetch the user from the database
       const user = await User.findById(req.user.userId).select("+role");
 
       if (!user) {
+        console.log("User not found.");
         return res.status(404).json({ message: "User not found." });
       }
 
+      // Decrypt the role
       const decryptedRole = cryptoHelper.decrypt(user.role);
 
+      // Check if the role is among the allowed roles
       if (!allowedRoles.includes(decryptedRole)) {
+        console.log("Access denied. Insufficient permissions.");
         return res.status(403).json({
           message: "Access denied. Insufficient permissions.",
         });
       }
 
+      // Allow access
       next();
     } catch (error) {
       console.error("Middleguard error:", error);
